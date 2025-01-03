06a2f189a7b3910dba8619179ed2ed7fe5d0e049
Michael Rotmanov <leachiM2k@leachiM2k.de>
2025-01-03 10:04:55 +0100

    chore: upgrade electron

--- forge.config.ts
+++ forge.config.ts
@@ -1,9 +1,4 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerDMG } from "@electron-forge/maker-dmg";
-import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
@@ -18,20 +13,40 @@ const config: ForgeConfig = {
     },
     rebuildConfig: {},
     makers: [
-        new MakerSquirrel({
-            setupIcon: './images/icon.ico',
-        }),
-        new MakerZIP({}, ['darwin']),
-        new MakerDMG({
-            icon: path.join(process.cwd(), '/images/icon.icns'),
-        }),
-        new MakerRpm({}),
-        new MakerDeb({
-            options: {
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                setupIcon: path.join(process.cwd(), '/images/icon.ico'),
+            },
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            platforms: ['linux'],
+            config: {
                 maintainer: 'Michael Rotmanov',
-                icon: './images/icon.png',
+                icon: path.join(process.cwd(), '/images/icon.png'),
+            },
+        },
+        {
+            name: '@electron-forge/maker-dmg',
+            platforms: ['darwin'],
+            config: {
+                icon: path.join(process.cwd(), '/images/icon.icns'),
             }
-        })],
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            platforms: ['linux'],
+            config: {
+                maintainer: 'Michael Rotmanov',
+                icon: path.join(process.cwd(), '/images/icon.png'),
+            }
+        }
+    ],
     plugins: [
         new AutoUnpackNativesPlugin({}),
         new WebpackPlugin({
